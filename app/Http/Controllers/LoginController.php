8e9bd4b171f760f055c4036ebb2f05e171c8d1db
ruchik <ruchikayasth143@gmail.com>
2024-06-02 12:50:37 +0530

    initial commit

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function index(){
+        return view('login');
+    }
+    
+    public function login(Request $request)
+    {
+        request()->validate([
+            'email' => 'required',
+            'psw' => 'required',
+        ]);
+
+        $credentials = $request->only('email', 'password');
+        if (isset($credentials)) {
+            // Authentication passed...
+            return view('welcome');
+        }else{
+            return Redirect::to("login")->withSuccess('Oppes! You have entered invalid credentials');
+        }        
+        // echo "<pre>";
+        // print_r($request->all());
+        // die('here');
+    }
+}
